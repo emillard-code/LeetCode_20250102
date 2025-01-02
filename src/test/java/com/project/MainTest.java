6c7454816ed1deb40b661582e0ab2399530415a2
Emerson Millard <emillard.code@gmail.com>
2025-01-02 12:00:00 -0800

    Added challenge link and made minor adjustments.

--- src/test/java/com/project/MainTest.java
+++ src/test/java/com/project/MainTest.java
@@ -1,9 +1,9 @@
 package com.project;
 
 import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import static com.github.stefanbirkner.systemlambda.SystemLambda.tapSystemOut;
-import static org.junit.jupiter.api.Assertions.*;
 
 public class MainTest {
 
