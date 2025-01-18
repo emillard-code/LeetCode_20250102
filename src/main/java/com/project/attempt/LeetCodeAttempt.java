32a550e4de46d2922b12fec9b62ff9839ccc717f
Emerson Millard <emillard.code@gmail.com>
2025-01-17 20:38:00 -0800

    Added comments to challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -16,36 +16,46 @@ public class LeetCodeAttempt {
 
     }
 
+    // This method will count the number of qualifying strings that start and end in a vowel
+    // within words[], providing a total count for each of the ranges provided by queries[][].
     public static int[] countVowelStringsInRange(String[] words, int[][] queries) {
 
-        int[] ans = new int[queries.length];
+        // The solution will be stored in answer[].
+        int[] answer = new int[queries.length];
 
         for (int a = 0; a < queries.length; a++) {
 
+            // We are only expecting two numbers within the single arrays. Return null otherwise.
             if (queries[a].length != 2) { return null; }
 
+            // For every pair of numbers provided by queries[][], we loop through words[] using
+            // their values as the starting index and ending index to loop through. And we check each
+            // string within that range to see if it is a valid string starting and ending in a vowel or not.
             for (int b = queries[a][0]; b < queries[a][1] + 1; b++) {
 
+                // For each match within the range, increment the value of answer[a].
                 if (vowelCheck(words[b])) {
-                    ans[a]++;
+                    answer[a]++;
                 }
 
             }
 
         }
 
-        return ans;
+        // After all the queries[][] ranges have been looped through, return the answer array.
+        return answer;
 
     }
 
+    // This is a helper method to check if a string starts and ends in a vowel.
     private static boolean vowelCheck(String word) {
 
         if (
                 word.charAt(0) != 'a' &&
-                word.charAt(0) != 'e' &&
-                word.charAt(0) != 'i' &&
-                word.charAt(0) != 'o' &&
-                word.charAt(0) != 'u'
+                        word.charAt(0) != 'e' &&
+                        word.charAt(0) != 'i' &&
+                        word.charAt(0) != 'o' &&
+                        word.charAt(0) != 'u'
         ) {
             return false;
         }
